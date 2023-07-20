0118e11de642b641507ac712e04bd84538127119
Sahba Hajihoseini <106279424+Sahbahh@users.noreply.github.com>
2023-07-19 18:34:12 -0700

    Update list.h

--- list.h
+++ list.h
@@ -1,115 +1,59 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef _CHAIN_H_
+#define _CHAIN_H_
 #include <stdbool.h>
 
-#define LIST_SUCCESS 0
-#define LIST_FAIL -1
+#define CHAIN_SUCCESS 0
+#define CHAIN_FAIL -1
 
 // Full types defined in .c file
-typedef struct Node_s Node;
-struct Node_s;
+typedef struct Segment_s Segment;
+struct Segment_s;
 
-typedef struct List_s List;
-struct List_s;
+typedef struct Chain_s Chain;
+struct Chain_s;
 
 
-// Maximum number of unique lists the system can support
-// (You may modify its value for your needs)
-#define LIST_MAX_NUM_HEADS 10
+// Maximum number of unique chains the system can support
+#define CHAIN_MAX_NUM_HEADS 10
 
-// Maximum total number of nodes (statically allocated) to be shared across all lists
-// (You may modify its value for your needs)
-#define LIST_MAX_NUM_NODES 100
+// Maximum total number of segments (statically allocated) to be shared across all chains
+#define CHAIN_MAX_NUM_SEGMENTS 100
 
-// General Error Handling:
-// Client code is assumed never to call these functions with a NULL List pointer, or 
-// bad List pointer. If it does, any behaviour is permitted (such as crashing).
-// HINT: Use assert(pList != NULL); just to add a nice check, but not required.
-
-// Makes a new, empty list, and returns its reference on success. 
+// Makes a new, empty chain, and returns its reference on success. 
 // Returns a NULL pointer on failure.
-List* List_create();
+Chain* Chain_create();
 
-// Returns the number of items in pList.
-int List_count(List* pList);
+// Returns the number of items in pChain.
+int Chain_count(Chain* pChain);
 
-// Returns a pointer to the first item in pList and makes the first item the current item.
-// Returns NULL and sets current item to NULL if list is empty.
-void* List_first(List* pList);
+// Functions for traversing the chain
+void* Chain_first(Chain* pChain);
+void* Chain_last(Chain* pChain); 
+void* Chain_next(Chain* pChain);
+void* Chain_prev(Chain* pChain);
+void* Chain_curr(Chain* pChain);
 
-// Returns a pointer to the last item in pList and makes the last item the current item.
-// Returns NULL and sets current item to NULL if list is empty.
-void* List_last(List* pList); 
+// Functions for adding items to the chain
+int Chain_add(Chain* pChain, void* pItem);
+int Chain_insert(Chain* pChain, void* pItem);
+int Chain_append(Chain* pChain, void* pItem);
+int Chain_prepend(Chain* pChain, void* pItem);
 
-// Advances pList's current item by one, and returns a pointer to the new current item.
-// If this operation advances the current item beyond the end of the pList, a NULL pointer 
-// is returned and the current item is set to be beyond end of pList.
-void* List_next(List* pList);
+// Function for removing item from the chain
+void* Chain_remove(Chain* pChain);
 
-// Backs up pList's current item by one, and returns a pointer to the new current item. 
-// If this operation backs up the current item beyond the start of the pList, a NULL pointer 
-// is returned and the current item is set to be before the start of pList.
-void* List_prev(List* pList);
+// Function for joining two chains
+void Chain_join(Chain* pChain1, Chain* pChain2);
 
-// Returns a pointer to the current item in pList.
-void* List_curr(List* pList);
+// Function for freeing a chain
+typedef void (*DESTRUCTOR_FN)(void* pItem);
+void Chain_free(Chain* pChain, DESTRUCTOR_FN pDestructorFn);
 
-// Adds the new item to pList directly after the current item, and makes item the current item. 
-// If the current pointer is before the start of the pList, the item is added at the start. If 
-// the current pointer is beyond the end of the pList, the item is added at the end. 
-// Returns 0 on success, -1 on failure.
-int List_add(List* pList, void* pItem);
+// Function for trimming a chain
+void* Chain_trim(Chain* pChain);
 
-// Adds item to pList directly before the current item, and makes the new item the current one. 
-// If the current pointer is before the start of the pList, the item is added at the start. 
-// If the current pointer is beyond the end of the pList, the item is added at the end. 
-// Returns 0 on success, -1 on failure.
-int List_insert(List* pList, void* pItem);
-
-// Adds item to the end of pList, and makes the new item the current one. 
-// Returns 0 on success, -1 on failure.
-int List_append(List* pList, void* pItem);
-
-// Adds item to the front of pList, and makes the new item the current one. 
-// Returns 0 on success, -1 on failure.
-int List_prepend(List* pList, void* pItem);
-
-// Return current item and take it out of pList. Make the next item the current one.
-// If the current pointer is before the start of the pList, or beyond the end of the pList,
-// then do not change the pList and return NULL.
-void* List_remove(List* pList);
-
-// Adds pList2 to the end of pList1. The current pointer is set to the current pointer of pList1. 
-// pList2 no longer exists after the operation; its head is available
-// for future operations.
-void List_concat(List* pList1, List* pList2);
-
-// Delete pList. pItemFreeFn is a pointer to a routine that frees an item. 
-// It should be invoked (within List_free) as: (*pItemFreeFn)(itemToBeFreedFromNode);
-// pList and all its nodes no longer exists after the operation; its head and nodes are 
-// available for future operations.
-// UPDATED: Changed function pointer type, May 19
-typedef void (*FREE_FN)(void* pItem);
-void List_free(List* pList, FREE_FN pItemFreeFn);
-
-// Return last item and take it out of pList. Make the new last item the current one.
-// Return NULL if pList is initially empty.
-void* List_trim(List* pList);
-
-// Search pList, starting at the current item, until the end is reached or a match is found. 
-// In this context, a match is determined by the comparator parameter. This parameter is a
-// pointer to a routine that takes as its first argument an item pointer, and as its second 
-// argument pComparisonArg. Comparator returns 0 if the item and comparisonArg don't match, 
-// or 1 if they do. Exactly what constitutes a match is up to the implementor of comparator. 
-// 
-// If a match is found, the current pointer is left at the matched item and the pointer to 
-// that item is returned. If no match is found, the current pointer is left beyond the end of 
-// the list and a NULL pointer is returned.
-// 
-// UPDATED: Added May 19
-// If the current pointer is before the start of the pList, then start searching from
-// the first node in the list (if any).
-typedef bool (*COMPARATOR_FN)(void* pItem, void* pComparisonArg);
-void* List_search(List* pList, COMPARATOR_FN pComparator, void* pComparisonArg);
+// Function for searching through a chain
+typedef bool (*MATCHER_FN)(void* pItem, void* pComparisonArg);
+void* Chain_search(Chain* pChain, MATCHER_FN pMatcher, void* pComparisonArg);
 
 #endif
